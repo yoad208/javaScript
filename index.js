479bba9425aa7713773018c64061865dc9d748b5
yoad208 <94039515+yoad208@users.noreply.github.com>
2022-06-26 01:59:22 +0300

    :100:

--- index.js
+++
@@ -1,9 +0,0 @@
-async function temp() {
-    await fetch('https://movie-quote-api.herokuapp.com/v1/quote')
-        .then(data => {
-            return data.json();
-        })
-        .then(update => {
-            console.log(update);
-        });
-}
